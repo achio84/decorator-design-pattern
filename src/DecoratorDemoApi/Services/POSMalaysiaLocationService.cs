e61816376f20951f32bc40d1a7246ad4eaef97e0
Jong <chingchong.jong@etiqa.com.my>
2023-10-18 10:50:10 +0800

    chore: cleanup

--- src/DecoratorDemoApi/Services/POSMalaysiaLocationService.cs
+++ src/DecoratorDemoApi/Services/POSMalaysiaLocationService.cs
@@ -1,12 +1,12 @@
-﻿using System.Text.Json;
-using DecoratorDemoApi.Models;
+﻿using DecoratorDemoApi.Models;
+using System.Text.Json;
 
 namespace DecoratorDemoApi.Services
 {
     internal class POSMalaysiaLocationService : ILocationService
     {
         private readonly IHttpClientFactory _factory;
-        private readonly Dictionary<string, List<Location>> _locations = new Dictionary<string, List<Location>>();
+
         public POSMalaysiaLocationService(IHttpClientFactory factory)
         {
             _factory = factory;
@@ -15,24 +15,16 @@ namespace DecoratorDemoApi.Services
         {
             var locations = new List<Location>();
 
-            if (_locations.ContainsKey(postcode))
+            var client = _factory.CreateClient();
+            var msg = new HttpRequestMessage(HttpMethod.Get, $"https://api.pos.com.my/PostcodeWebApi/api/Postcode?Postcode={postcode}");
+
+            var result = await client.SendAsync(msg);
+
+            if (result.IsSuccessStatusCode)
             {
-                return _locations.GetValueOrDefault(postcode);
-            }
-            else
-            {
-                var client = _factory.CreateClient();
-                var msg = new HttpRequestMessage(HttpMethod.Get, $"https://api.pos.com.my/PostcodeWebApi/api/Postcode?Postcode={postcode}");
+                var response = await result.Content.ReadAsStringAsync();
 
-                var result = await client.SendAsync(msg);
-
-                if (result.IsSuccessStatusCode)
-                {
-                    var response = await result.Content.ReadAsStringAsync();
-
-                    locations = JsonSerializer.Deserialize<List<Location>>(response);
-                    _locations.Add(postcode, locations);
-                }
+                locations = JsonSerializer.Deserialize<List<Location>>(response);
             }
 
             return locations;
